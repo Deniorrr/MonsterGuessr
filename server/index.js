96090e8013469eee3436f713f6ea1c7467ee146b
Deniorrr <kujonek95@gmail.com>
2025-07-14 13:34:16 +0200

    opened cors

--- server/index.js
+++ server/index.js
@@ -47,19 +47,23 @@ const allowedOrigins = [
 
 app.use((req, res, next) => {
   const origin = req.headers.origin;
-  if (origin && !allowedOrigins.includes(origin)) {
-    return res.status(403).send("Forbidden: Origin not allowed");
-  }
-  if (origin && allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Vary", "Origin");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET,POST,PUT,DELETE,OPTIONS"
-    );
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
-  }
+  // if (origin && !allowedOrigins.includes(origin)) {
+  //   return res.status(403).send("Forbidden: Origin not allowed");
+  // }
+  // if (origin && allowedOrigins.includes(origin)) {
+  //   res.setHeader("Access-Control-Allow-Origin", origin);
+  //   res.setHeader("Access-Control-Allow-Credentials", "true");
+  //   res.setHeader("Vary", "Origin");
+  //   res.setHeader(
+  //     "Access-Control-Allow-Methods",
+  //     "GET,POST,PUT,DELETE,OPTIONS"
+  //   );
+  //   res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  // }
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+
   if (req.method === "OPTIONS") {
     return res.sendStatus(204);
   }
